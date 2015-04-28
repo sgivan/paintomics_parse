3eba6fb4cca726bf61880007212905d5083d470f
Scott A. Givan <sgivan@yahoo.com>
2015-04-27 20:57:35 -0500

    Start script to join multiple tables into a single table for Paintomics

---
+++ bin/join_tables.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE:  join_tables.pl
+#
+#        USAGE:  ./join_tables.pl  
+#
+#  DESCRIPTION: Script to join any number of tables
+#               given as arguments on command line
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Dr. Scott A. Givan (sag), givans@missouri.edu
+#      COMPANY:  University of Missouri, USA
+#      VERSION:  1.0
+#      CREATED:  04/24/15 17:04:33
+#     REVISION:  ---
+#===============================================================================
+
+use 5.010;      # Require at least Perl version 5.10
+use autodie;
+use Getopt::Long; # use GetOptions function to for CL args
+use warnings;
+use strict;
+use Data::Table;
+
+my ($debug,$verbose,$help);
+
+my $result = GetOptions(
+    "debug"     =>  \$debug,
+    "verbose"   =>  \$verbose,
+    "help"      =>  \$help,
+);
+
+if ($help) {
+    help();
+    exit(0);
+}
+
+sub help {
+
+    say <<HELP;
+
+
+HELP
+
+}
+
+
+
