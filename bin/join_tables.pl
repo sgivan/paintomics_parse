973a5aafd985b78af9a2e6b6c979123e0415cd29
Scott A. Givan <sgivan@yahoo.com>
2015-04-29 17:00:30 -0500

    join_tables.pl works. Tested with two tables.

--- bin/join_tables.pl
+++ bin/join_tables.pl
@@ -41,12 +41,41 @@ if ($help) {
 
 sub help {
 
-    say <<HELP;
+say <<HELP;
 
+    usage: join_tables.pl [options] table_one table_two table_three ...
+   
+    debug
+    verbose
+    help
 
 HELP
 
 }
 
+open(my $OUT, ">", "new_table.tsv");
 
+my @tables = ();
+
+for my $file (@ARGV) {
+    say STDERR $file if ($debug);
+
+    my $table = Data::Table::fromFile($file);
+    push(@tables,$table);
+}
+
+say STDERR "\@tables contains " . scalar(@tables) . " tables" if ($debug);
+
+my $joined_table;
+for (my $tbl = 0; $tbl < scalar(@tables); ++$tbl) {
+    my $table = $tables[$tbl];
+    last unless ($tables[++$tbl]);# NOTE: this increments $tbl by 1
+    unless ($table->isEmpty()) {
+        # note that $tbl has been incremented outside of loop control statement
+        $joined_table = $table->join($tables[$tbl],Data::Table::FULL_JOIN,['Gene_ID'],['Gene_ID']);
+    }
+     
+}
+say STDERR "\$joined_table isa '" . ref($joined_table) . "'" if ($debug);
+say $OUT $joined_table->tsv();
 
